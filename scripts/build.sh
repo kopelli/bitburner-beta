5e182922cd59ba94277256fd51883cdb0eddac9e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-07 20:45:07 +0000

    Deploy bitburner-beta to github.com/kopelli/bitburner-beta.git:master

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,6 @@
 set -ev
 if [ "${TRAVIS}" = "true" && "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     if [ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "dev" ]; then
-        # Running on Travis, but not triggered by a pull request. Assume it's a branch
         git checkout ${TRAVIS_BRANCH}
     fi
 fi
