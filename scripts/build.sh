06633bc67d8f63edc12ccbdf242a52274fb992e7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-08 14:54:45 +0000

    Deploy bitburner-beta to github.com/kopelli/bitburner-beta.git:master

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS}" = "true" && "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    if [ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "dev" ]; then
+UPDATE_BUILD_ARTIFACTS="false";
+if [ "${TRAVIS}" = "true" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    if [[ "${TRAVIS_BRANCH}" =~ ^(master|dev|travis-ci)$ ]]; then
+        UPDATE_BUILD_ARTIFACTS="true";
         git checkout ${TRAVIS_BRANCH}
     fi
 fi
 
 npm run build
 
-if [ "${TRAVIS}" = "true" && "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    git add -u
-    git commit -m "Updating bundles from ${TRAVIS_JOB_NUMBER}"
-    git push
-fi
+#if [ "$UPDATE_BUILD_ARTIFACTS" = "true" ]; then
+#    git add -u
+#    git commit -m "Updating bundles from ${TRAVIS_JOB_NUMBER}" --author="Deployment Bot (from Travis CI) <deploy@travis-ci.org>"
+#    git push
+#fi
